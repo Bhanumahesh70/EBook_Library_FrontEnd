d3cae60f6b74ef89d054f3c5a9cd7749dde09bc6
Bhanumahesh Pasham <bhanumaheshpasham@Mac-2432.cable.rcn.com>
2025-04-06 01:39:23 -0500

    Added generic form entity and refactored category entity to use it

---
+++ src/components/Categories/AddCategoryForm.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import {
+  addCategory,
+  updateCategoryById,
+} from '../../services/categoryService';
+import { useParams, useLocation, useNavigate } from 'react-router-dom';
+import FeedBackModal from '../Modals/FeedBackModal';
+import { Category } from '../../services/types';
+import {
+  handleFormSubmit,
+  handleInputOnChange,
+} from '../../services/formUtilities';
+import TextInputField from '../Form/TextInputField';
+import TextAreaField from '../Form/TextAreaField';
+import { getCategoryById } from '../../services/categoryService';
+import { textInModal, handleModalClosing } from '../../services/modalUtilities';
+const CategoryForm = () => {
+  const defaultCategory: Category = {
+    id: '',
+    categoryName: '',
+    description: '',
+  };
+  const [category, setCategory] = React.useState<Category>(defaultCategory);
+  const { id } = useParams();
+  const isEditing = Boolean(id);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const categoryTobeEdit = location.state?.category;
+  const [showModal, setShowModal] = React.useState(false);
+  const [showFeedbackModal, setShowFeedbackModel] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isEditing) {
+      if (categoryTobeEdit) {
+        setCategory({
+          id: categoryTobeEdit.id,
+          categoryName: categoryTobeEdit.categoryName,
+          description: categoryTobeEdit.description,
+        });
+      } else {
+        getCategoryById(id).then((data) => setCategory(data));
+      }
+    } else {
+      setCategory(defaultCategory);
+    }
+  }, [id]);
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    handleFormSubmit<Category>({
+      e,
+      isEditing,
+      entity: category,
+      id,
+      updateFunction: updateCategoryById,
+      addFunction: addCategory,
+      entityName: 'Category',
+      setIsError,
+      setShowModal: setShowFeedbackModel,
+    });
+  }
+  function handleChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    handleInputOnChange<Category>(e, setCategory);
+  }
+
+  function displayTextInModal() {
+    return textInModal({ isError, isEditing, entityName: 'Author' });
+  }
+  function handleCloseFeedBackModal() {
+    handleModalClosing<Category>({
+      setShowModal,
+      isError,
+      isEditing,
+      url: '/ebook/categories',
+      setEntity: setCategory,
+      entity: defaultCategory,
+    });
+  }
+  return (
+    <div className="formHeader">
+      <h1>{isEditing ? 'Edit Category' : 'Add New Category'}</h1>
+      <div className="container mb-5 formContainer">
+        <form className="entityform" onSubmit={handleSubmit}>
+          <TextInputField
+            label=" Name"
+            id="categoryName"
+            value={category.categoryName}
+            onChange={handleChange}
+          />
+          <TextAreaField
+            label=" Description"
+            id="description"
+            value={category.description}
+            onChange={handleChange}
+          />
+
+          <div className="m-3">
+            <button>Submit</button>
+          </div>
+        </form>
+        <FeedBackModal
+          showFeedBackModal={showFeedbackModal}
+          displayTextInFeedbackModal={displayTextInModal}
+          close={handleCloseFeedBackModal}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CategoryForm;
--- src/components/Form/EntityForm.tsx
+++ src/components/Form/EntityForm.tsx
@@ -1,8 +1,99 @@
 import React from 'react';
-import { useParams, useNavigate, Link } from 'react-router-dom';
+import { useParams, useNavigate, Link, useLocation } from 'react-router-dom';
 import FeedBackModal from '../Modals/FeedBackModal';
-const EntityForm = () => {
-  return <div>EntityForm</div>;
-};
+import { textInModal, handleModalClosing } from '../../services/modalUtilities';
+import {
+  handleFormSubmit,
+  handleInputOnChange,
+} from '../../services/formUtilities';
 
+interface EntityFormProps<T> {
+  defaultEntity: T;
+  getEntityById: (id: string | undefined) => Promise<T>;
+  entityName: string;
+  updateEntity: (id: string | undefined, entity: T) => Promise<T>;
+  addEntity: (entity: T) => Promise<T>;
+  urlToNavitageAwayFromForm: string;
+  renderFields: (
+    entity: T,
+    handleChange: (e: React.ChangeEvent<any>) => void
+  ) => React.ReactNode;
+}
+function EntityForm<T>({
+  defaultEntity,
+  getEntityById,
+  entityName,
+  updateEntity,
+  addEntity,
+  urlToNavitageAwayFromForm,
+  renderFields,
+}: EntityFormProps<T>) {
+  const [entity, setEntity] = React.useState<T>(defaultEntity);
+  const { id } = useParams();
+  const isEditing = Boolean(id);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = React.useState(false);
+  const [showFeedbackModal, setShowFeedbackModel] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isEditing) {
+      getEntityById(id).then((data) => setEntity(data));
+    } else {
+      setEntity(defaultEntity);
+    }
+  }, [id]);
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    handleFormSubmit<T>({
+      e,
+      isEditing,
+      entity,
+      id,
+      updateFunction: updateEntity,
+      addFunction: addEntity,
+      entityName,
+      setIsError,
+      setShowModal: setShowFeedbackModel,
+    });
+  }
+
+  function handleChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    handleInputOnChange<T>(e, setEntity);
+  }
+  function displayTextInModal() {
+    return textInModal({ isError, isEditing, entityName });
+  }
+  function handleCloseFeedBackModal() {
+    handleModalClosing<T>({
+      setShowModal,
+      isError,
+      isEditing,
+      url: urlToNavitageAwayFromForm,
+      setEntity,
+      entity: defaultEntity,
+    });
+  }
+  return (
+    <div className="formHeader">
+      <h1>{isEditing ? `Edit ${entityName}` : `Add New ${entityName}`}</h1>
+      <div className="container mb-5 formContainer">
+        <form className="entityform" onSubmit={handleSubmit}>
+          {renderFields(entity)}
+          <div className="m-3">
+            <button>Submit</button>
+          </div>
+        </form>
+        <FeedBackModal
+          showFeedBackModal={showFeedbackModal}
+          displayTextInFeedbackModal={displayTextInModal}
+          close={handleCloseFeedBackModal}
+        />
+      </div>
+    </div>
+  );
+}
 export default EntityForm;
